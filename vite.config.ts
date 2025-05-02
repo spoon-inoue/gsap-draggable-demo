eca40dfb25c5d034cb5978488d7fd3de96f9510c
spoon-inoue <h-inoue@studio-spoon.co.jp>
2025-05-02 23:25:07 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
 	root: './src',
 	publicDir: resolve(__dirname, 'public'),
-	base: '/draggable-demo/',
+	base: '/gsap-draggable-demo/',
 	plugins: [glsl(), tsconfigPaths()],
 	server: {
 		host: true,
